e56929874bfd57c3f86f7e8892ee02e35965b53b
LUNCH131BALDING <77846093+LUNCH131BALDING@users.noreply.github.com>
2021-04-07 21:08:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var database;
+var position;
+var player,form,game;
+var gameState=0
+var playerCount
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+game=new Game()
+  game.getState()
+    game.start()
+
+    
+  
+
+   
+
+
+
+}
+
+function draw(){
+  background("white");
+  
+    
+    
+  
+}
+
+
+
+  
